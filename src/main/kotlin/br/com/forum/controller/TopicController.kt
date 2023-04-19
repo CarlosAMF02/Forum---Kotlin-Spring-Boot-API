1ffee56c2272b28e2d46d563eb5b9ba66cc14c1e
CarlosAMF02 <c.a.mascarenhas@hotmail.com>
2023-04-19 12:12:22 -0300

    [Topic] [CHORE] GetById method and endpoint

--- src/main/kotlin/br/com/forum/controller/TopicController.kt
+++ src/main/kotlin/br/com/forum/controller/TopicController.kt
@@ -1,10 +1,9 @@
 package br.com.forum.controller
 
-import br.com.forum.model.Course
 import br.com.forum.model.Topic
-import br.com.forum.model.User
 import br.com.forum.service.TopicService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
@@ -15,6 +14,11 @@ class TopicController (private val topicService: TopicService) {
 
     @GetMapping
     fun list(): List<Topic> {
-        return topicService.list();
+        return topicService.list()
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable(name = "id") id: Long): Topic {
+        return topicService.getById(id)
     }
 }
--- src/main/kotlin/br/com/forum/service/TopicService.kt
+++ src/main/kotlin/br/com/forum/service/TopicService.kt
@@ -7,12 +7,23 @@ import org.springframework.stereotype.Service
 
 
 @Service
-class TopicService {
-    fun list(): List<Topic> {
+class TopicService(private var topics: List<Topic>) {
+
+    init {
         val user = User(id = 1, name = "Carlos", email = "carlos@email.com")
         val course = Course(id = 1, name = "Kotlin - Spring Boot", category = "Programação")
-        val topic = Topic(id = 1, title = "Dúvida Kotlion", message = "Variáveis no Kotlin", course = course, author = user)
+        val topic1 = Topic(id = 1, title = "Dúvida Kotlin", message = "Variáveis no Kotlin", course = course, author = user)
+        val topic2 = Topic(id = 2, title = "Dúvida Kotlin", message = "Tipo var e val", course = course, author = user)
+        val topic3 = Topic(id = 3, title = "Dúvida Kotlin", message = "IDE", course = course, author = user)
 
-        return listOf(topic, topic, topic)
+        topics = listOf(topic1, topic2, topic3)
+    }
+
+    fun list(): List<Topic> {
+        return topics
+    }
+
+    fun getById(id: Long): Topic {
+        return topics.stream().filter { t -> t.id == id }.findFirst().get()
     }
 }
